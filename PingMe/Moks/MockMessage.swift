8d3a8b1e883c572fc948e68f1d423f8f2325b20d
Alexandr Khakana <Kalashiq.org@gmail.com>
2025-12-13 15:25:33 +0200

    refactor: rename Message mock to MockMessage to avoid conflicts
    - Rename PingMe/Moks/Message.swift to MockMessage.swift
    - Resolve 'Multiple commands produce' build error

--- PingMe/Moks/MockMessage.swift
+++ PingMe/Moks/MockMessage.swift
@@ -1,6 +1,7 @@
 import Foundation
 
-struct Message: Identifiable {
+// MARK: - Mock Message (Legacy - not used anymore)
+struct MockMessage: Identifiable {
     let id = UUID()
     let content: String
     let timestamp: Date
